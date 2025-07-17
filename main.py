f83e2486967ff009ebca4c1d1dc0899becee1984
Obiadullah <126314333+Obiadullah@users.noreply.github.com>
2025-07-17 14:43:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("7711618135:AAGb8F2a3bPCjIZp--eZ0Ym1cHpwVqtnkXI")
+BOT_TOKEN = "7711618135:AAGb8F2a3bPCjIZp--eZ0Ym1cHpwVqtnkXI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 স্বাগতম! আমি O.R Cash Bot!")
